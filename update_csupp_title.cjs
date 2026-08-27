61c7d67e0d86becf5af9232c99fde60726c0f64c
Aryan <your-github-aryantiwarihynm@gmail.com>
2026-08-27 23:33:53 +0530

    chore: update branding title and favicon

---
+++ update_csupp_title.cjs
@@ -0,0 +1,8 @@
+﻿const fs = require('fs');
+let code = fs.readFileSync('index.html', 'utf8');
+code = code.replace(
+  '<title>vendor-portal</title>',
+  '<title>ProcGen | Vendor Portal</title>'
+);
+fs.writeFileSync('index.html', code, 'utf8');
+console.log("Updated title in Csupplier");
